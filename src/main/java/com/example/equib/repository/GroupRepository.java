cd2dad4e596407c8360c0ada9e0da4e5c9395033
Fkremariam Nigatu <fkremariamn21@gmail.com>
2025-09-15 07:01:46 +0000

    second commit

---
+++ src/main/java/com/example/equib/repository/GroupRepository.java
@@ -0,0 +1,8 @@
+package com.example.equib.repository;
+
+
+import com.example.equib.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
--- src/repository/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.equib.repository;
-
-
-import com.example.equib.model.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
